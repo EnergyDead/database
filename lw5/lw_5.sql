13e199944b7622e753b57da8ca188347d2f39c61
Ruslan Vasilev <96041711+EnergyDead@users.noreply.github.com>
2022-05-30 12:11:23 +0300

    Update lw_5.sql

--- lw5/lw_5.sql
+++ lw5/lw_5.sql
@@ -1,33 +1,33 @@
--- 1. Добавить внешние ключи.
+-- 1. Р”РѕР±Р°РІРёС‚СЊ РІРЅРµС€РЅРёРµ РєР»СЋС‡Рё.
 ALTER TABLE room ADD FOREIGN KEY(id_room_category) REFERENCES room_category(id_room_category);
 ALTER TABLE room ADD FOREIGN KEY(id_hotel) REFERENCES hotel(id_hotel);
 ALTER TABLE room_in_booking ADD FOREIGN KEY(id_room) REFERENCES room(id_room);
 ALTER TABLE room_in_booking ADD FOREIGN KEY(id_booking) REFERENCES booking(id_booking);
 ALTER TABLE booking ADD FOREIGN KEY(id_client) REFERENCES client(id_client);
 
--- 2. Выдать информацию о клиентах гостиницы “Космос”, проживающих в номерах категории “Люкс” на 1 апреля 2019г.
+-- 2. Р’С‹РґР°С‚СЊ РёРЅС„РѕСЂРјР°С†РёСЋ Рѕ РєР»РёРµРЅС‚Р°С… РіРѕСЃС‚РёРЅРёС†С‹ вЂњРљРѕСЃРјРѕСЃвЂќ, РїСЂРѕР¶РёРІР°СЋС‰РёС… РІ РЅРѕРјРµСЂР°С… РєР°С‚РµРіРѕСЂРёРё вЂњР›СЋРєСЃвЂќ РЅР° 1 Р°РїСЂРµР»СЏ 2019Рі.
 SELECT client.id_client, client.name, client.phone FROM client 
   LEFT JOIN booking ON client.id_client = booking.id_client
   LEFT JOIN room_in_booking ON booking.id_booking = room_in_booking.id_booking
   LEFT JOIN room ON room_in_booking.id_room = room.id_room
   LEFT JOIN hotel ON room.id_hotel = hotel.id_hotel
   LEFT JOIN room_category ON room.id_room_category = room_category.id_room_category
-  WHERE hotel.name = 'Космос' AND room_category.name = 'Люкс' AND room_in_booking.checkin_date <= '2019-04-01' AND room_in_booking.checkout_date >= '2019-04-01';
+  WHERE hotel.name = 'РљРѕСЃРјРѕСЃ' AND room_category.name = 'Р›СЋРєСЃ' AND room_in_booking.checkin_date <= '2019-04-01' AND room_in_booking.checkout_date >= '2019-04-01';
 
--- 3. Дать список свободных номеров всех гостиниц на 22 апреля.
+-- 3. Р”Р°С‚СЊ СЃРїРёСЃРѕРє СЃРІРѕР±РѕРґРЅС‹С… РЅРѕРјРµСЂРѕРІ РІСЃРµС… РіРѕСЃС‚РёРЅРёС† РЅР° 22 Р°РїСЂРµР»СЏ.
 SELECT room.id_room FROM room
   LEFT JOIN room_in_booking ON room.id_room = room_in_booking.id_room
   WHERE room_in_booking.id_room IS NULL OR NOT (room_in_booking.checkin_date <= '2019-04-22' AND room_in_booking.checkout_date >= '2019-04-22');
 
--- 4. Дать количество проживающих в гостинице “ Космос ” на 23 марта по каждой категории номеров
+-- 4. Р”Р°С‚СЊ РєРѕР»РёС‡РµСЃС‚РІРѕ РїСЂРѕР¶РёРІР°СЋС‰РёС… РІ РіРѕСЃС‚РёРЅРёС†Рµ вЂњ РљРѕСЃРјРѕСЃ вЂќ РЅР° 23 РјР°СЂС‚Р° РїРѕ РєР°Р¶РґРѕР№ РєР°С‚РµРіРѕСЂРёРё РЅРѕРјРµСЂРѕРІ
 SELECT room_category.name, COUNT(*) AS clients_quantity FROM room_category 
   LEFT JOIN room ON room.id_room_category = room_category.id_room_category
   LEFT JOIN room_in_booking ON room_in_booking.id_room = room.id_room
   LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
-  WHERE hotel.name = 'Космос' AND (room_in_booking.checkin_date <= '2019-03-23' AND room_in_booking.checkout_date >= '2019-03-23')
+  WHERE hotel.name = 'РљРѕСЃРјРѕСЃ' AND (room_in_booking.checkin_date <= '2019-03-23' AND room_in_booking.checkout_date >= '2019-03-23')
   GROUP BY room_category.name;
 
--- 5. Дать список последних проживавши х клиентов по всем комнатам гостиницы “Космос”, выехавшим в апреле с указанием даты выезда
+-- 5. Р”Р°С‚СЊ СЃРїРёСЃРѕРє РїРѕСЃР»РµРґРЅРёС… РїСЂРѕР¶РёРІР°РІС€Рё С… РєР»РёРµРЅС‚РѕРІ РїРѕ РІСЃРµРј РєРѕРјРЅР°С‚Р°Рј РіРѕСЃС‚РёРЅРёС†С‹ вЂњРљРѕСЃРјРѕСЃвЂќ, РІС‹РµС…Р°РІС€РёРј РІ Р°РїСЂРµР»Рµ СЃ СѓРєР°Р·Р°РЅРёРµРј РґР°С‚С‹ РІС‹РµР·РґР°
 SELECT DISTINCT client.name, room_in_booking.id_room FROM client
   LEFT JOIN booking ON client.id_client = booking.id_client
   LEFT JOIN room_in_booking ON booking.id_booking = room_in_booking.id_booking
@@ -36,25 +36,25 @@ SELECT DISTINCT client.name, room_in_booking.id_room FROM client
 	  LEFT JOIN room_in_booking ON room_in_booking.id_booking = booking.id_booking
 	  LEFT JOIN room ON room.id_room = room_in_booking.id_room
 	  LEFT JOIN hotel ON room.id_hotel = hotel.id_hotel
-	  WHERE hotel.name = 'Космос' AND room_in_booking.checkout_date >= '2019-04-01' AND room_in_booking.checkout_date <= '2019-04-30') AS tbl
+	  WHERE hotel.name = 'РљРѕСЃРјРѕСЃ' AND room_in_booking.checkout_date >= '2019-04-01' AND room_in_booking.checkout_date <= '2019-04-30') AS tbl
 	  GROUP BY tbl.id_room
   ) AS last_room_date ON last_room_date.checkout_date = room_in_booking.checkout_date AND last_room_date.id_room = room_in_booking.id_room;
 
--- 6. Продлить на 2 дня дату проживания в гостинице “ Космос ” всем клиентам комнат категории “ Бизнес ”, которые заселились 10 мая.
+-- 6. РџСЂРѕРґР»РёС‚СЊ РЅР° 2 РґРЅСЏ РґР°С‚Сѓ РїСЂРѕР¶РёРІР°РЅРёСЏ РІ РіРѕСЃС‚РёРЅРёС†Рµ вЂњ РљРѕСЃРјРѕСЃ вЂќ РІСЃРµРј РєР»РёРµРЅС‚Р°Рј РєРѕРјРЅР°С‚ РєР°С‚РµРіРѕСЂРёРё вЂњ Р‘РёР·РЅРµСЃ вЂќ, РєРѕС‚РѕСЂС‹Рµ Р·Р°СЃРµР»РёР»РёСЃСЊ 10 РјР°СЏ.
 UPDATE room_in_booking SET room_in_booking.checkout_date = DATEADD(DAY, 2, room_in_booking.checkout_date) FROM room_in_booking
   LEFT JOIN room ON room.id_room = room_in_booking.id_room
   LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
   LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
-  WHERE hotel.name = 'Космос' AND room_category.name = 'Бизнес' AND room_in_booking.checkin_date = '2019-05-10';
+  WHERE hotel.name = 'РљРѕСЃРјРѕСЃ' AND room_category.name = 'Р‘РёР·РЅРµСЃ' AND room_in_booking.checkin_date = '2019-05-10';
 
--- Проверочка
+-- РџСЂРѕРІРµСЂРѕС‡РєР°
 SELECT room_in_booking.checkout_date FROM room_in_booking
   LEFT JOIN room ON room.id_room = room_in_booking.id_room
   LEFT JOIN hotel ON hotel.id_hotel = room.id_hotel
   LEFT JOIN room_category ON room_category.id_room_category = room.id_room_category
-  WHERE hotel.name = 'Космос' AND room_category.name = 'Бизнес' AND room_in_booking.checkin_date = '2019-05-10';
+  WHERE hotel.name = 'РљРѕСЃРјРѕСЃ' AND room_category.name = 'Р‘РёР·РЅРµСЃ' AND room_in_booking.checkin_date = '2019-05-10';
 
--- 7. Найти все "пересекающиеся варианты проживания.
+-- 7. РќР°Р№С‚Рё РІСЃРµ "РїРµСЂРµСЃРµРєР°СЋС‰РёРµСЃСЏ РІР°СЂРёР°РЅС‚С‹ РїСЂРѕР¶РёРІР°РЅРёСЏ.
 SELECT * FROM room_in_booking AS tbl1
   INNER JOIN room_in_booking AS tbl2 ON ((tbl2.checkin_date >= tbl1.checkin_date AND tbl2.checkin_date <= tbl1.checkout_date) OR
   (tbl2.checkout_date >= tbl1.checkin_date AND tbl2.checkout_date <= tbl1.checkout_date) OR
@@ -66,18 +66,18 @@ SELECT * FROM room_in_booking AS tbl1
  where (tbl2.checkin_date <= tbl1.checkin_date AND tbl2.checkout_date >= tbl1.checkin_date) OR
   (tbl2.checkin_date >= tbl1.checkin_date AND tbl2.checkin_date <= tbl1.checkout_date);
 
--- 8. Создать бронированиев транзакции
+-- 8. РЎРѕР·РґР°С‚СЊ Р±СЂРѕРЅРёСЂРѕРІР°РЅРёРµРІ С‚СЂР°РЅР·Р°РєС†РёРё
 
 BEGIN TRANSACTION
-  INSERT INTO client (name, phone) VALUES ('Алексей Попов', '7(99)999-99-99');
-  INSERT INTO booking (id_client, booking_date) (SELECT id_client, '2021-03-17' FROM client WHERE name = 'Алексей Попов' AND phone = '7(99)999-99-99');
+  INSERT INTO client (name, phone) VALUES ('РђР»РµРєСЃРµР№ РџРѕРїРѕРІ', '7(99)999-99-99');
+  INSERT INTO booking (id_client, booking_date) (SELECT id_client, '2021-03-17' FROM client WHERE name = 'РђР»РµРєСЃРµР№ РџРѕРїРѕРІ' AND phone = '7(99)999-99-99');
   INSERT INTO room_in_booking (id_booking, id_room, checkin_date, checkout_date)
     (SELECT id_booking, 10, '2021-03-18', '2021-03-20' FROM booking WHERE id_booking =
     (SELECT id_booking FROM booking WHERE id_client =
-    (SELECT id_client FROM client WHERE name = 'Алексей Попов' AND phone = '7(99)999-99-99')));
+    (SELECT id_client FROM client WHERE name = 'РђР»РµРєСЃРµР№ РџРѕРїРѕРІ' AND phone = '7(99)999-99-99')));
 COMMIT TRANSACTION;
 
--- 9. Добавить необходимые индексы для всех таблиц
+-- 9. Р”РѕР±Р°РІРёС‚СЊ РЅРµРѕР±С…РѕРґРёРјС‹Рµ РёРЅРґРµРєСЃС‹ РґР»СЏ РІСЃРµС… С‚Р°Р±Р»РёС†
 CREATE INDEX idx_room_category ON room_category (name);
 CREATE INDEX idx_hotel ON hotel (name);
 CREATE INDEX idx_checkin_date ON room_in_booking (checkin_date);
